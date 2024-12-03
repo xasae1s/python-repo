ef32313cb20dfed2d8e58301dea9bc22c328c8c1
xasae1s <sehgaladitya8271@gmail.com>
2024-12-04 02:14:57 +0530

    MY CALCULATOR

---
+++ 1. calculator.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the First Num:"))
+num2=int(input("Enter the Second Num:"))
+add=num1+num2
+print("The sum of", num1, "and", num2, "is:",add)
+sub=num1-num2
+print("The difference of",num1,"and", num2, "is:", sub)
