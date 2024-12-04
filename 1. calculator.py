43a8a9bdf0957392d80deb16d5f6c99065de3c02
xasae1s <sehgaladitya8271@gmail.com>
2024-12-05 00:12:34 +0530

    ADDED NEW OPERATIONS

--- 1. calculator.py
+++ 1. calculator.py
@@ -1,6 +1,15 @@
 num1=int(input("Enter the First Num:"))
 num2=int(input("Enter the Second Num:"))
 add=num1+num2
-print("The sum of", num1, "and", num2, "is:",add)
+print("The Sum Of", num1, "and", num2, "is:",add)
 sub=num1-num2
-print("The difference of",num1,"and", num2, "is:", sub)
+print("The Difference Of",num1,"and", num2, "is:", sub)
+division=num1/num2
+print("The Division Of", num1, "and", num2, "is:",division)
+Multiply=num1*num2
+print("The Product Of", num1, "and",num2 ,"is:", Multiply)
+fdivision=num1//num2
+print("The Floadivision Of",num1, "and",num2, "is:", fdivision)
+modulus=num1**num2
+print("The Modulus Of", num1, "and",num2, "is:",modulus)
+print("\nTHANKS FOR USING MY CALCULATOR")
