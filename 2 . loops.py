e7d64c47be01b24c1cc99feca099109d0983a68c
xasae1s <sehgaladitya8271@gmail.com>
2024-12-05 00:26:49 +0530

    FIRST LOOP PROGRAM

--- 2 . loops.py
+++ 2 . loops.py
@@ -2,6 +2,7 @@ ApplePrice=60
 Budget=50
 if(ApplePrice<=Budget):
     print("YOU CAN BUT APPLES")
+elif(ApplePrice>20):
+    print("YOU STILL CANNOT BUT THESE APPLES")
 else:
     print("YOU CANNOT BUY APPLES")
-    
